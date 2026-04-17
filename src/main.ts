fd1608312cc4246145977248a3b21c40b4b2715b
MayconRodrigues <maycon.rodrigues@novoseguros.com.br>
2026-04-17 14:43:17 -0300

    excluindo prisma

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module.js';
 
+//precisa adicionar CORS para conectar com front End
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);  
   await app.listen(3002);
